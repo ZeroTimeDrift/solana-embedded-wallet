c1302733dadb70de374407696bb4902826a5dbdc
ZeroTimeDrift <403.hevar@gmail.com>
2023-06-19 17:04:50 +0100

    rename folder to wallet

--- solana-embedded-wallet/pages/index.js
+++ solana-embedded-wallet/pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { WalletMultiButton, WalletDisconnectButton } from '@solana/wallet-adapter-react-ui';
-import { initialize } from './../components/initalize.ts';
+import { initialize } from './../wallet/initalize.ts';
 
 export default function Home() {
   initialize();
